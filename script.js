d42914173ea721852432aafd8c3bc768cc73810f
prabowo02 <prabowo1048576@gmail.com>
2022-05-22 23:43:48 +0800

    Add bg music and disable double-tap zoom

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const FRAME_NUM = 17;
 const PUNCH_AUDIO = new Audio('punch.mp3');
-var currentFrame = 0;
-
 const PUNCH_FRAMES = [4, 8, 12, 16].reduce((o, v) => ({...o, [v]: new Audio('punch.mp3')}), {});
+const BG_AUDIO = new Audio('bg.mp3');
+
+var currentFrame = 0;
 
 function getFileName(frame) {
   if (frame < 10) return '0' + String(frame) + '.png';
@@ -21,16 +22,37 @@ function nextFrame() {
   }
 }
 
+var allowAdvance = true;
+function advance() {
+  if (!allowAdvance) return;
+  startBgm();
+  nextFrame();
+  setTimeout(() => {
+    nextFrame();
+    allowAdvance = true;
+  }, 4.0 / 30)
+}
+
+var startedBgm = false;
+function startBgm() {
+  if (!startedBgm) {
+    BG_AUDIO.volume = 0.5;
+    BG_AUDIO.loop = true;
+    BG_AUDIO.play();
+    startedBgm = true;
+  }
+}
+
 function init() {
   var main = document.getElementById("main");
   for (let i = 0; i < FRAME_NUM; ++i) {
     var img = document.createElement("img");
     img.src = getFileName(i);
-    if (i > 0) img.classList.add("hidden");
     img.id = String(i);
+    if (i > 0) img.classList.add("hidden");
     main.appendChild(img);
   }
-  document.addEventListener("mousedown", nextFrame);
-  document.addEventListener("keydown", nextFrame);
-  document.addEventListener("touchstart", nextFrame);
+  document.addEventListener("mousedown", advance);
+  document.addEventListener("keydown", advance);
+  document.addEventListener("touchstart", advance);
 }
